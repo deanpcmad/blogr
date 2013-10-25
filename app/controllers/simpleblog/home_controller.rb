f98dcb4856b3fd80f013dc6a8a438a059091c9c3
Dean Perry <dean@fusemail.me>
2013-10-25 20:31:09 +0100

    use the rails app's application layout and show a list of posts on home#index

--- app/controllers/simpleblog/home_controller.rb
+++ app/controllers/simpleblog/home_controller.rb
@@ -1,10 +1,13 @@
 module Simpleblog
   class HomeController < ApplicationController
 
-    def index
+  	# Use the main Rails app's application layout
+  	layout "application"
 
+    # Display a list of posts on the home page
+    def index
+			@posts = Post.published.order("published_at DESC")
     end
 
   end
 end
-
