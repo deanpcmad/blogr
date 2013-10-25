f83ef65132aecf954069d16896b8a7ddbba064a4
Nando Sousa <nandosousafr@gmail.com>
2013-10-25 00:10:42 -0300

    removed future spec feature

--- spec/models/simpleblog/post_spec.rb
+++ spec/models/simpleblog/post_spec.rb
@@ -6,7 +6,6 @@ module Simpleblog
     it { should validate_uniqueness_of(:title) }
     it { should validate_presence_of(:content) }
     it { should validate_presence_of(:title) }
-    it { should validate_numericality_of(:user_id) }
     it { should ensure_inclusion_of(:status).in_array(['draft', 'public']) }
     it { should have_and_belong_to_many :categories }
     it { should have_many :images }
