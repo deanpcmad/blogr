b7a345b2d3e55f01895dcab45eeb99c0638f129d
Dean Perry <dean@fusemail.me>
2013-10-29 20:38:44 +0000

    permalink in admin

--- app/controllers/simpleblog/admin/posts_controller.rb
+++ app/controllers/simpleblog/admin/posts_controller.rb
@@ -74,7 +74,7 @@ module Simpleblog
     private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params[:id])
+      @post = Post.find_by_permalink!(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
